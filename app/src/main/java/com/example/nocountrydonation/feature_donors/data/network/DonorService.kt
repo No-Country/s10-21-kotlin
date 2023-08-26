98c91c49d560024a5504bf68cc39bb7cf20316d4
SantiagoG <santigime972@gmail.com>
2023-08-26 18:24:19 -0300

    show donors and request in order by date

--- app/src/main/java/com/example/nocountrydonation/feature_donors/data/network/DonorService.kt
+++ app/src/main/java/com/example/nocountrydonation/feature_donors/data/network/DonorService.kt
@@ -3,12 +3,13 @@ package com.example.nocountrydonation.feature_donors.data.network
 import com.example.nocountrydonation.feature_donors.domain.Donors
 import com.example.nocountrydonation.util.ResultState
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class DonorService {
     suspend fun getDonor(): ResultState<List<Donors>> {
         val listService = mutableListOf<Donors>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("donantes").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("donantes").orderBy("created_at", Query.Direction.DESCENDING).get().await()
         for (service in querySnapshot.documents) {
             val item = service.toObject(Donors::class.java)
             item?.image = service["image"].toString()
